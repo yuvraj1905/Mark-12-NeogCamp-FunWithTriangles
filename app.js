52af27985ae3ca59b2bd44846444d690b152cb53
yuvraj1905 <yuvrajkumar2509@gmail.com>
2022-10-03 10:08:27 +0530

    fixed: angle value accepting 0

--- app.js
+++ app.js
@@ -13,6 +13,10 @@ function checkIsTriangle() {
     alert("Please enter valid angles for all 3 sides ");
     return;
   }
+  if (angleOne.value < 1 || angleTwo.value < 1 || angleThree.value < 1) {
+    alert("Angle value can't be 0 or negative.");
+    return;
+  }
 
   const sum =
     Number(angleOne.value) + Number(angleThree.value) + Number(angleTwo.value);
